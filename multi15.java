9a035d8c575a1862f962c03d396c0c16b38fc796
smit_kalathiya <63186967+smit02K@users.noreply.github.com>
2020-11-05 14:45:43 +0530

    Add files via upload

---
+++ multi15.java
@@ -0,0 +1,14 @@
+public class multi15 {
+
+    public static void main(String[] args) {
+    
+    int i=1;                                                   
+
+    while(i<11){
+
+        System.out.println("15x" + i +"=" + 15*i );
+        i++;
+    }
+  }
+    
+}
