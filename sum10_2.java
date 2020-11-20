a3a0e3c367c3947c909d64c916a1c1fc7afae2c4
smit_kalathiya <63186967+smit02K@users.noreply.github.com>
2020-11-20 09:09:31 +0530

    Add files via upload

---
+++ sum10_2.java
@@ -0,0 +1,19 @@
+
+public class sum10_2  {
+
+    public static void main(String[] args) {
+
+        int i=0,sum=0;
+
+        
+        do{
+            i=i+2;
+            sum=sum+i;
+            
+        }while(i!=20);
+
+        System.out.print("sum of first 10 even number is: " + sum);
+
+
+    }
+}
